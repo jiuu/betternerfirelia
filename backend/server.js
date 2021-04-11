e40adc439e5eff6086f310d725e4fc4e4726136a
Matthew G <mgagnon1@live.com>
2021-04-11 14:31:02 -0400

    updated frontend
    updated stuff

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const testRouter = require('./routes/testroute');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+const riot_api_key = process.env.RIOT_API_KEY;
 
 app.use(cors());
 app.use(express.json());
+app.use('/testRoute', require('./routes/testroute'));
 
-const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true});
 const connection = mongoose.connection;
 
@@ -19,6 +21,12 @@ connection.once('open', () => {
 });
 
 const championsRouter = require('./routes/champions');
+
+app.get('/', (req,res) => {
+	res.json({api_key: riot_api_key})
+})
+
+app.get('/testRoute', testRouter);
 app.use('/champions', championsRouter);
 
 
--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/navbar.component"
-import ChampionList from "./components/champion-list.component"
-
+import Navbar from "./components/navbar.component";
+import ChampionList from "./components/champion-list.component";
+import SearchSummoner from "./components/searchsummoner.component"
+import "./App.css"
 
 function App() {
   return (
     <Router>
-        <div className="container">
-            <Navbar />
-            <br/>
-            <Route path="/" exact component={ChampionList} />
+      <Navbar />
+        <div className="testContainer">
+        </div>
+        <div className="summonerNameContainer">
+            <SearchSummoner />
         </div>
     </Router>
   );
