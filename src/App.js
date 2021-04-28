8e96670526a09803dcf4be2b5268192a3f1a63df
jiuu <straisin@gmail.com>
2021-04-28 12:37:53 -0400

    mostly formatting changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar.component";
 import ChampionList from "./components/champion-list.component";
 import SearchSummoner from "./components/searchsummoner.component";
+<<<<<<< Updated upstream
 import "../src/styles/App.css";
 
 function App() {
@@ -24,6 +30,36 @@ function App() {
             <SearchSummoner />
           </div>
       </Router>
+=======
+import Home from './components/home.component';
+import "./styles/appstyles.css";
+import MyNav from './components/navbar.component';
+import About from './components/about.component';
+import Contact from './components/contact.component';
+
+function App() {
+  return (
+    <div class = "mainBody">
+      
+      <MyNav />
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/search">
+            <SearchSummoner />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
+>>>>>>> Stashed changes
     </div>
   );
 }
