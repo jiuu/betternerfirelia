176d693a9be04313b32fc71f8b8e8c526eae0ff7
MATTHEW GAGNON <mgnon@bu.edu>
2021-04-28 20:40:47 -0400

    Refactored classes to functional components

--- src/components/searchsummoner.component.js
+++ src/components/searchsummoner.component.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import{ Button, Form, Dropdown} from 'react-bootstrap'
 import '../styles/searchsummonerstyles.css';
 
 const SearchSummoner = () => {
     return (
-        <div className = "Search">
-        <form>
-            <label>
-                <input type="text" name="getSummonerName" placeholder="Ex: From Iron"/>
-            </label>
-            <input type="submit" value="Search" />
-        </form>
+        <div className = "searchClass">
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Summoner name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter a summoner name" />
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                </Form.Group>
+                <div className="dropDownServerSelect">
+                    <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Select a region
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item >NA</Dropdown.Item>
+                        <Dropdown.Item>EUW</Dropdown.Item>
+                        <Dropdown.Item >EUNE</Dropdown.Item>
+                    </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
